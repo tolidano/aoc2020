c5d5137113c80b1583d3bf5d4d5c54316c668486
Shawn Tolidano <shawn.tolidano@onepeloton.com>
2020-12-03 00:09:51 -0500

    3 part 1 and 2 (part 1 was just check(1, 3)

--- 3.php
+++ 3.php
@@ -1,21 +1,30 @@
 <?php
 
-$i = file_get_contents("3.in");
-$r = explode("\n", $i);
-$c = 0;
+function check($down, $right) {
+    $i = file_get_contents("3.in");
+    $r = explode("\n", $i);
+    $t = 0;
+    $c = count($r);
+    $rw = 0;
+    $width = strlen(trim($r[0]));
+    $pos = 0;
 
-foreach ($r as $l) {
-    if (!trim($l)) { // blank lines
-        continue;
+    while ($rw < $c) {
+        $rw += $down;
+        $pos += $right;
+        if ($pos >= $width) {
+            $pos -= $width;
+        }
+        if (substr($r[$rw], $pos, 1) == '#') {
+            $t++;
+        }
     }
-
-    $p = explode(":", $l); // split on :
-    $pw = trim($p[1]); // trim the space and have just the pw (after the colon)
-    $rq = explode(' ', $p[0]); // the part before the colon was x-y c
-    $pwp = count_chars($pw, 1); // magic in php to get character counts by ascii code
-    if ($pwp[ord($chr)] >= $min && $pwp[ord($chr)] <= $max) { // just check if the character appears in range
-        $c++;
-    }
+    return $t;
 }
 
-echo $c;
+$a = check(1, 1);
+$t = check(1, 3);
+$c = check(1, 5);
+$d = check(1, 7);
+$m = check(2, 1);
+echo $a * $t * $c * $d * $m;
