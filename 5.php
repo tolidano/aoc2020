c0db2a1fe75efe704a001a2f3fe5814d7e017976
Shawn Tolidano <shawn.tolidano@onepeloton.com>
2020-12-05 00:23:43 -0500

    Day 5 done

--- 5.php
+++ 5.php
@@ -2,20 +2,46 @@
 
 $i = file_get_contents("5.in");
 $r = explode("\n", $i);
-$c = 0;
+$m = 0;
+$sids = [];
 
 foreach ($r as $l) {
-    if (!trim($l)) { // blank lines
+    if (!trim($l)) {
         continue;
     }
-
-    $p = explode(":", $l); // split on :
-    $pw = trim($p[1]); // trim the space and have just the pw (after the colon)
-    $rq = explode(' ', $p[0]); // the part before the colon was x-y c
-    $pwp = count_chars($pw, 1); // magic in php to get character counts by ascii code
-    if ($pwp[ord($chr)] >= $min && $pwp[ord($chr)] <= $max) { // just check if the character appears in range
-        $c++;
+    $minrow = 0;
+    $maxrow = 127;
+    $minseat = 0;
+    $maxseat = 7;
+    for ($j = 0; $j < 7; $j++) {
+        if (substr($l, $j, 1) == 'F') {
+            $maxrow = $minrow + (int)(($maxrow - $minrow) / 2);
+        } else {
+            $minrow = $minrow + (int)(($maxrow - $minrow) / 2) + 1; 
+        }
+    }
+    for ($k = 7; $k < 10; $k++) {
+        if (substr($l, $k, 1) == 'L') {
+            $maxseat = $minseat + (int)(($maxseat - $minseat) / 2);
+        } else {
+            $minseat = $minseat + (int)(($maxseat - $minseat) / 2); 
+        }
+    }
+    $seatId = ($maxrow * 8) + $maxseat;
+    $sids[$seatId] = 1;
+    if ($m < $seatId) {
+        $m = $seatId;
     }
 }
 
-echo $c;
+echo $m . "\n";
+
+$check = false;
+for ($n = 0; $n < $m; $n++) {
+    if (isset($sids[$n])) {
+        $check = true;
+    }
+    if ($check && !isset($sids[$n]) && isset($sids[$n - 1]) && isset($sids[$n + 1])) {
+        echo $n . "\n";
+    }
+}
