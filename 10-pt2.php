c65f5d7f28c41a0aa00d546079268a7d4361c241
Shawn Tolidano <shawn.tolidano@onepeloton.com>
2020-12-10 00:49:12 -0500

    Fuck it.

---
+++ 10-pt2.php
@@ -0,0 +1,24 @@
+<?php
+ini_set('memory_limit', -1);
+$i = file_get_contents("10.in");
+$r = explode("\n", $i);
+
+sort($r);
+array_shift($r);
+$start = 0;
+$pop = 0;
+
+for ($i = count($r) - 2; $i >= 1; $i--) {
+    $diff = (int)$r[$i + 1] - (int)$r[$i - 1];
+    echo "AT INDEX: $i LOOKING AT {$r[$i]} WHICH HAS {$r[$i - 1]} AND {$r[$i + 1]} SURROUNDING WITH DIFF $diff - ";
+    if ($diff < 3) {
+        $pop++;
+        echo "INCREMENTED $pop";
+    }
+    echo "\n";
+}
+echo pow(2, $pop) . "\n";
+echo pow(2, $pop + 1) . "\n";
+echo pow(2, $pop - 1) . "\n";
+echo pow(2, $pop - 2) . "\n";
+echo pow(2, $pop - 3) . "\n";
--- 10.php
+++ 10.php
@@ -2,21 +2,21 @@
 $i = file_get_contents("10.in");
 $r = explode("\n", $i);
 
-for ($i = 25; $i < count($r); $i++) {
-    $target = $r[$i];
-    $found = false;
-    for ($j = $i - 25; $j < $i; $j++) {
-        for ($k = $i - 25; $k < $i; $k++) {
-            if ($k != $j) {
-                $sum = $r[$j] + $r[$k];
-                if ($sum == $target) {
-                    $found = true;
-                }
-            }
-        }
+sort($r);
+$start = 0;
+$threediff = 1;
+$onediff = 0;
+foreach ($r as $l) {
+    if (!trim($l)) {
+        continue;
     }
-    if (!$found) {
-        echo $target;
-        die;
+    if ((int)$l == $start + 1) {
+        $onediff++;
+    } elseif ((int)$l == $start + 3) {
+        $threediff++;
+    } else {
     }
+    $start = (int)$l;
 }
+
+echo $onediff * $threediff;
