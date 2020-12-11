e6c8600a206a638bfcce2055ce8acb2194985756
Shawn Tolidano <shawn.tolidano@onepeloton.com>
2020-12-11 00:34:15 -0500

    Trying

--- 10-pt2.php
+++ 10-pt2.php
@@ -11,14 +11,15 @@ $pop = 0;
 for ($i = count($r) - 2; $i >= 1; $i--) {
     $diff = (int)$r[$i + 1] - (int)$r[$i - 1];
     echo "AT INDEX: $i LOOKING AT {$r[$i]} WHICH HAS {$r[$i - 1]} AND {$r[$i + 1]} SURROUNDING WITH DIFF $diff - ";
-    if ($diff < 3) {
+    if ($diff <= 3) {
         $pop++;
         echo "INCREMENTED $pop";
     }
     echo "\n";
 }
-echo pow(2, $pop) . "\n";
+echo pow(2, $pop + 2) . "\n";
 echo pow(2, $pop + 1) . "\n";
+echo pow(2, $pop) . "\n";
 echo pow(2, $pop - 1) . "\n";
 echo pow(2, $pop - 2) . "\n";
 echo pow(2, $pop - 3) . "\n";
---
+++ 11.php
@@ -0,0 +1,82 @@
+<?php
+$r = explode("\n", file_get_contents("11.in"));
+$blank = array_pop($r);
+$moves = 0;
+while ($moves < 1 || $m != $r) {
+    $m = $r;
+    for ($i = 0; $i < count($m); $i++) {
+        for ($j = 0; $j < strlen($m[0]); $j++) {
+            if (substr($m[$i], $j, 1) == ".") {
+                continue;
+            }
+            $occupied = 0;
+            if ($i > 0 && $j > 0 && substr($m[$i - 1], $j - 1, 1) == "#") {
+                $occupied++; // UL
+            }
+            if ($i < count($m) - 2 && $j > 0 && substr($m[$i + 1], $j - 1, 1) == "#") {
+                $occupied++; // DL
+            }
+            if ($i > 0 && substr($m[$i - 1], $j, 1) == "#") {
+                $occupied++; // U
+            }
+            if ($i < count($m) - 2 && substr($m[$i + 1], $j, 1) == "#") {
+                $occupied++; // D
+            }
+            if ($i < count($m) - 2 && $j < strlen($m[0]) - 2 && substr($m[$i + 1], $j + 1, 1) == "#") {
+                $occupied++; // DR
+            }
+            if ($i > 0 && $j < strlen($m[0]) - 2 && substr($m[$i - 1], $j + 1, 1) == "#") {
+                $occupied++; // UR
+            }
+            if ($j > 0 && substr($m[$i], $j - 1, 1) == "#") {
+                $occupied++; // L
+            }
+            if ($j < strlen($m[0]) - 2 && substr($m[$i], $j + 1, 1) == "#") {
+                $occupied++; // R
+            }
+            if ($occupied == 0 && substr($m[$i], $j, 1) == "L") {
+                if ($j > 0 && $j < strlen($m[$i]) - 1) {
+                    $r[$i] = substr($m[$i], 0, $j) . "#" . substr($m[$i], $j + 1);
+                    //echo "X1\n";
+                } elseif ($j == 0) {
+                    $r[$i] = "#" . substr($m[$i], 1);
+                    //echo "X2\n";
+                } else {
+                    $r[$i] = substr($m[$i], 0, -1) . "#";
+                    //echo "X3\n";
+                }
+                $ml = strlen($m[$i]);
+                $rl = strlen($r[$i]);
+                //echo "OLDSIT: {$m[$i]} ($ml)\nNEWSIT: {$r[$i]} ($rl)\n";
+                if ($rl != 92) { die; }
+            }
+            if ($occupied >= 4 && substr($m[$i], $j, 1 ) == "#") {
+                if ($j > 0 && $j < strlen($m[$i]) - 1) {
+                    $r[$i] = substr($m[$i], 0, $j) . "L" . substr($m[$i], $j + 1);
+                    //echo "Y1\n";
+                } elseif ($j == 0) {
+                    $r[$i] = "L" . substr($m[$i], 1);
+                    //echo "Y2\n";
+                } else {
+                    $r[$i] = substr($m[$i], 0, -1) . "L";
+                    //echo "Y3\n";
+                }
+                $ml = strlen($m[$i]);
+                $rl = strlen($r[$i]);
+                //echo "OLDSTAND: {$m[$i]} ($ml)\nNEWSTAND: {$r[$i]} ($rl)\n";
+                if ($rl != 92) { die; }
+            }
+        }
+    } 
+    $moves++;
+}
+echo "STEADY\n";
+$occupied = 0;
+for ($i = 0; $i < count($m); $i++) {
+    for ($j = 0; $j < strlen($m[0]); $j++) {
+        if (substr($m[$i], $j, 1) == "#") {
+            $occupied++;
+        } 
+    }
+}
+echo "OCCUPIED: $occupied\n";
