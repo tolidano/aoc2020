d1cee5d54e1f747dd2efcd9d4a421b03f992a0c7
Shawn Tolidano <shawn.tolidano@onepeloton.com>
2020-12-12 00:58:36 -0500

    Giving up

--- 12-pt2.php
+++ 12-pt2.php
@@ -11,62 +11,80 @@ $r = [
 'F11',
 ];
  */
-$start_x = 0;
-$start_y = 0;
-$face = 1;
-$dirs = ['N', 'E', 'S', 'W'];
+$wp_x = 10;
+$wp_y = 1;
+$ship_x = 0;
+$ship_y = 0;
 
 foreach ($r as $l) {
     $dir = substr($l, 0, 1);
     $amt = (int)substr($l, 1);
     echo "Parsed $l into $dir and $amt ";
     if ($dir == 'E') {
-        $start_x += $amt;
-        echo "Moved X to $start_x\n";
+        $wp_x += $amt;
+        echo "Moved X to $wp_x\n";
     } elseif ($dir == 'N') {
-        $start_y += $amt;
-        echo "Moved Y to $start_y\n";
+        $wp_y += $amt;
+        echo "Moved Y to $wp_y\n";
     } elseif ($dir == 'S') {
-        $start_y -= $amt;
-        echo "Moved Y to $start_y\n";
+        $wp_y -= $amt;
+        echo "Moved Y to $wp_y\n";
     } elseif ($dir == 'W') {
-        $start_x -= $amt;
-        echo "Moved X to $start_x\n";
+        $wp_x -= $amt;
+        echo "Moved X to $wp_x\n";
     } elseif ($dir == 'F') {
-        switch ($dirs[$face]) {
-            case 'E':
-                $start_x += $amt;
-                echo "Moved FEX to $start_x\n";
-                break;
-            case 'N':
-                $start_y += $amt;
-                echo "Moved FNY to $start_y\n";
-                break;
-            case 'S':
-                $start_y -= $amt;
-                echo "Moved FSY to $start_y\n";
-                break;
-            case 'W':
-                $start_x -= $amt;
-                echo "Moved FWX to $start_x\n";
-                break;
-        }
-    } elseif ($dir == 'L') {
+        echo "Ship: $ship_x $ship_y WP: $wp_x $wp_y ";
+        $ship_x += $amt * $wp_x;
+        $ship_y += $amt * $wp_y;
+        echo "Ship Now: $ship_x $ship_y\n";
+    } elseif ($dir == 'L' || $dir == 'R') {
         $rot = $amt % 360;
-        $num = $rot / 90;
-        echo "FACE: $face ";
-        $face = ($face - $num) % 4;
-        if ($face < 0) {
-            $face += 4;
+        echo "WPX: $wp_x WPY: $wp_y ";
+        if ($wp_x >= 0 && $wp_y >= 0) {
+            $q = 1;
+        } elseif ($wp_x >= 0 && $wp_y <= 0) {
+            $q = 2;
+        } elseif ($wp_x <= 0 && $wp_y <= 0) {
+            $q = 3;
+        } elseif ($wp_x <= 0 && $wp_y >= 0) {
+            $q = 4;
         }
-        echo "ROT: $rot NUM: $num NEWFACE: $face\n";
-    } elseif ($dir == 'R') {
-        $rot = $amt % 360;
-        $num = $rot / 90;
-        echo "FACE: $face ";
-        $face = ($face + $num) % 4;
-        echo "ROT: $rot NUM: $num NEWFACE: $face\n";
+        if ($rot == 180) {
+            $wp_x *= -1;
+            $wp_y *= -1;
+        } else {
+            //90 or 270, L or R
+            $dq = ($q + ($dir == 'L' ? 1 : -1)) % 4;
+            if ($dq < 1) {
+                $dq += 4;
+            }
+            echo "Q: $q DQ: $dq "; 
+            if ($q == 1 && $dq == 2) {
+                $t = $wp_x;
+                $wp_x = -1 * $wp_y;
+                $wp_y = $t;
+            } elseif ($q == 1 && $dq == 4) {
+                $t = $wp_x;
+                $wp_x = $wp_y;
+                $wp_y = -1 * $t;
+            } elseif ($q == 2 && $dq == 1) {
+                $t = $wp_x;
+                $wp_x = $wp_y;
+                $wp_y = $t * -1;
+            } elseif ($q == 2 && $dq == 3) {
+                $t = $wp_x;
+                $wp_x = $wp_y;
+                $wp_y = -1 * $t;
+            } elseif ($q == 3 && $dq == 2) {
+                $t = $wp_x;
+                $wp_x = $wp_y;
+                $wp_y = -1 * $t;
+            } else {
+                echo "UNHANDLED! $q $dq ";
+            }
+        }
+        echo "ROT: $rot NUM: $num NWPX: $wp_x NWPY: $wp_y\n";
     }
 }
 
-echo "END: $start_x $start_y\n";
+echo "END: $wp_x $wp_y $ship_x $ship_y\n";
