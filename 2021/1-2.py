0a45fdc285dba46ac85b099879ad6163959e1636
Shawn Tolidano <shawn.tolidano@onepeloton.com>
2021-12-02 09:44:16 -0500

    X

--- 2021/1-2.py
+++ 2021/1-2.py
@@ -4,9 +4,6 @@ with open("1.in", "r") as fh:
     inc = 0
     for i in range(len(lines) - 3):
         group = int(lines[i]) + int(lines[i+1]) + int(lines[i+2])
-        print(lines[i:i+3])
-        print(group)
-        print(prev)
         if group > prev and i > 0:
             inc += 1
         prev = group
