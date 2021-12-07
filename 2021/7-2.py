2bd08392eaece57a3bfcfe9121dbbe6a1e64f493
Shawn Tolidano <shawn.tolidano@onepeloton.com>
2021-12-07 00:27:30 -0500

    7

---
+++ 2021/7-2.py
@@ -0,0 +1,21 @@
+with open("7.in", "r") as fh:
+    lines = fh.readlines()
+    pos = lines[0].split(",")
+    mpos = 0
+    for f in range(len(pos)):
+        pos[f] = int(pos[f])
+        if pos[f] > mpos:
+            mpos = pos[f]
+    print(pos)
+    print(mpos)
+    minf = 1000000000000
+    mini = 0
+    for i in range(mpos):
+        f = 0
+        for m in pos:
+            x1 = abs(m - i)
+            f += (x1 * (x1 + 1)) / 2
+        if f < minf:
+            minf = f
+            mini = i
+    print(f"min f: {minf} at pos: {mini}")
