60f989c066bb425c2ea56292eedaa83747d1bf54
Shawn Tolidano <shawn.tolidano@onepeloton.com>
2021-12-07 22:04:53 -0500

    shorter

--- 2021/6.php
+++ 2021/6.php
@@ -1,22 +1,17 @@
 <?php
 $r = file_get_contents("6.in");
 $c = count_chars($r, 1);
-$d = 1;
-$x[0] = 0;
 foreach ($c as $k => $v) {
     if ($k > 48) $x[$k - 48] = $v;
 }
-while ($d <= 256) {
+for ($d = 0; $d < 256; $d++) {
     $a = $x[0];
-    $k = array_keys($x);
-    $y = [];
-    foreach ($k as $m) {
+    foreach (array_keys($x) as $m) {
         if ($m) $y[$m - 1] = $x[$m];
     }
     $x = $y;
+    unset($y);
     $x[6] += $a;
     $x[8] += $a;
-    $s = array_sum($x);
-    $d++;
 }
-echo $s;
+echo array_sum($x);
